f26669c86fede27371b4b71d0d0ba65a1c6f5efe
Cristian <crirompov@alum.us.es>
2018-09-14 19:42:20 +0200

    Actualizo script
    para la que Workbench acepte el formato.

--- Servidor/DDBB/Datos_para_pruebas.sql
+++ Servidor/DDBB/Datos_para_pruebas.sql
@@ -2,18 +2,18 @@ INSERT INTO perfiles (nombre, descripcion) VALUES ('Cuidador', 'Persona que ');
 
 INSERT INTO personas (dni, nombre, apellidos, fecha_nacimiento) VALUES ('58963322D', 'Xavi','el_amo', STR_TO_DATE( '04-11-1996', '%d-%m-%Y' ));
 
-INSERT INTO Cuidadores(persona) VALUES(1);
+INSERT INTO cuidadores(persona) VALUES(1);
 
-INSERT INTO Usuarios (username, password, cuidador_id,E_perfil) VALUES ('skaterpro360', 'nomirar',1,1);
+INSERT INTO usuarios (username, password, cuidador_id,E_perfil) VALUES ('skaterpro360', 'nomirar',1,1);
 
-INSERT INTO Dependientes (alias, persona, cuidador) Values ('abu',1,1);
+INSERT INTO dependientes (alias, persona, cuidador) Values ('abu',1,1);
 
-INSERT INTO SPDs VALUES ();
+INSERT INTO spds VALUES ();
 
-INSERT INTO Tratamientos (dependiente, spd) VALUES (1,1);
+INSERT INTO tratamientos (dependiente, spd) VALUES (1,1);
 
-INSERT INTO Medicamentos (nombre) VALUES ('LSD');
+INSERT INTO medicamentos (nombre) VALUES ('LSD');
 
-INSERT INTO Cargas (spd, medicamento, cantidad) VALUES (1,1,3);
+INSERT INTO cargas (spd, medicamento, cantidad) VALUES (1,1,3);
 
-INSERT INTO Dosis (hora_inicio, veces_dia, medicamento, tratamiento) VALUES (TIME("12:00:00"), 5, 1, 1);
+INSERT INTO dosis (hora_inicio, veces_dia, medicamento, tratamiento) VALUES (TIME("12:00:00"), 5, 1, 1);
