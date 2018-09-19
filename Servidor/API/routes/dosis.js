133746eecf73647f973f1abb88f65fb65c5e1845
Cristian <crirompov@alum.us.es>
2018-09-19 13:42:00 +0200

    Actualización con la que podemos acceder a las dosis de un dependiente concreto

--- Servidor/API/routes/dosis.js
+++ Servidor/API/routes/dosis.js
@@ -15,43 +15,70 @@ const router = express.Router()
 			2) Una vez encontrados, recorro todos los tratamientos y busco las Dosis asociadas
 			3) Una vez encontrada las Dosis, las introduzco en el Array creado al principio y lo devolvemos tras de todo.
 		*/
-		router.get('/:spd', (req, res, next) => {
+		router.get('/spd/:spd', (req, res, next) => {
 			var codeSpd = parseInt(req.params.spd)
-			//var dosisRet = new Array();
 			console.log(Date() + " - GET /dosis/" + codeSpd)
 			Tratamiento.findAll({ where: { ID_SPD: codeSpd } }).then(projects => {
 				projects.forEach(e => {
 					Dosis.findAll({where: {ID_TRATAMIENTO: e.ID}}).then(p => {
-						console.log("p --> "+p)
 						res.send(p)
-						/* p.forEach(dosis => {
-							res.send(dosis)
-							//dosisRet.push(dosis)
-						}) */
 					})
 				})
 		  })
-		  //Descomentar cada uno de lo comentado cuando consigamos que vayan los arrays
-		  //.then(
-		//	  res.send(dosisRet)
-		 // )
-		  
 		})
 
-		router.post('/:spd', (req, res, next) => {
+		router.post('/spd/:spd', (req, res, next) => {
 			console.log(Date() + " - POST /dosis/" + req.params.spd)
 			res
 			.sendStatus(405)
 		})
 
-		router.put('/:spd', (req, res, next) => {
+		router.put('/spd/:spd', (req, res, next) => {
 			console.log(Date() + " - PUT /dosis/" + req.params.spd)
 			res
 			.sendStatus(405)
 		})
 
-		router.delete('/:spd', (req, res, next) => {
+		router.delete('/spd/:spd', (req, res, next) => {
 			console.log(Date() + " - DELETE /dosis/" + req.params.spd)
+			res
+			.sendStatus(405)
+		})
+
+//Dosis por DEPENDIENTE:
+
+		/*
+			0) Creamos el array dosisRet para devolver los datos
+			1) Busco los tratamientos asociados al SPD.
+			2) Una vez encontrados, recorro todos los tratamientos y busco las Dosis asociadas
+			3) Una vez encontrada las Dosis, las introduzco en el Array creado al principio y lo devolvemos tras de todo.
+		*/
+		router.get('/dependiente/:id_dependiente', (req, res, next) => {
+			var id_dependiente_param = parseInt(req.params.id_dependiente)
+			console.log(Date() + " - GET /dosis/dependiente/" + id_dependiente_param)
+			Tratamiento.findAll({ where: { ID_DEPENDIENTE: id_dependiente_param } }).then(projects => {
+				projects.forEach(e => {
+					Dosis.findAll({where: {ID_TRATAMIENTO: e.ID}}).then(p => {
+						res.send(p)
+					})
+				})
+		  })
+		})
+
+		router.post('/dependiente/:id_dependiente', (req, res, next) => {
+			console.log(Date() + " - POST /dosis/dependiente/" + req.params.id_dependiente)
+			res
+			.sendStatus(405)
+		})
+
+		router.put('/dependiente/:id_dependiente', (req, res, next) => {
+			console.log(Date() + " - PUT /dosis/dependiente/" + req.params.id_dependiente)
+			res
+			.sendStatus(405)
+		})
+
+		router.delete('/dependiente/:id_dependiente', (req, res, next) => {
+			console.log(Date() + " - DELETE /dosis/dependiente/" + req.params.id_dependiente)
 			res
 			.sendStatus(405)
 		})
